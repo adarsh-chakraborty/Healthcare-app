59df2999e9886b096c858386f31c7e6a51ecb995
Adarsh Chakraborty <adarshchak108@gmail.com>
2022-09-06 22:21:53 +0530

    refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,30 @@
- import './App.css';
+import './App.css';
 import React from 'react';
-import {Route, BrowserRouter,Routes} from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Resources from './components/Resources';
-import UserSearch from './components/UserSearch';
-//import VideoCall from './components/VideoCall'
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Diabetes from './components/Diabetes.js';
-import Home from './components/Home';
 import LoginUi from './components/LoginUi';
-import HospitalRegistration from './components/HospitalRegistration';
 import UserRegistration from './components/UserRegistration';
-import Symptom from './components/Symptom'
-import HomePage from './components/HomePage'
-import PCOSPrediction from './components/PCOSprediction';
+import Symptom from './components/Symptom';
+import HomePage from './components/HomePage';
 import DepressionForm from './components/DepressionForm';
 import ChronicDiagnosis from './components/chronicDiagnosis';
-
 
 function App() {
   return (
     <BrowserRouter>
-     
-       <Routes>
-      
-       <Route path="/" exact element={<HomePage />} />
-       <Route path="/home" exact element={<HomePage/>}/>
-       <Route path="/usersearch" element={<UserSearch />} />
-       <Route path="/resources" element={<Resources />} />
-       
-       <Route path="/diabetes" element={<Diabetes/>}/>
-       <Route path="LoginUi" element={<LoginUi/>}/>
-       <Route path="/HospitalRegistration" element={<HospitalRegistration />} /> 
-       <Route path="/UserRegistration" element={<UserRegistration />} /> 
-       
-      
-       <Route path="/symptoms" element={<Symptom/>} />
-       <Route path="/pcos" element={<PCOSPrediction/>} />
-       <Route path="/depression" element={<DepressionForm/>} />
-       <Route path="/chronic" element={<ChronicDiagnosis/>} />
-       
-         
-       </Routes>
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+        <Route path="/home" exact element={<HomePage />} />
+
+        <Route path="/diabetes" element={<Diabetes />} />
+        <Route path="LoginUi" element={<LoginUi />} />
+
+        <Route path="/UserRegistration" element={<UserRegistration />} />
+
+        <Route path="/symptoms" element={<Symptom />} />
+        <Route path="/depression" element={<DepressionForm />} />
+        <Route path="/chronic" element={<ChronicDiagnosis />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,63 +1,86 @@
-import React from 'react'
-import { NavLink,Link } from 'react-router-dom';
-import { useAuth0 } from "@auth0/auth0-react";
-import {setGlobalState,useGlobalState} from './state';
-const Navbar = () =>{
-   const isloggedIn=useGlobalState("LoggedIn")[0]
-   const userRole=useGlobalState("token")[0]['role']
-   console.log(isloggedIn)
-    return (
-        <>
-        <nav style={{backgroundColor:'white',fontSize:'13px'}} class="navbar navbar-expand-md  ">
-  
-  <Link style={{color:'black'}}class="nav-link" to="/">Home</Link>
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import { setGlobalState, useGlobalState } from './state';
+const Navbar = () => {
+  const isloggedIn = useGlobalState('LoggedIn')[0];
+  const userRole = useGlobalState('token')[0]['role'];
+  console.log(isloggedIn);
+  return (
+    <>
+      <nav
+        style={{ backgroundColor: 'white', fontSize: '13px' }}
+        class="navbar navbar-expand-md  "
+      >
+        <Link style={{ color: 'black' }} class="nav-link" to="/">
+          Home
+        </Link>
 
-  
-  <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span style={{backgroundColor:"black"}} class="navbar-toggler-icon"></span>
-  </button>
+        <button
+          class="navbar-toggler "
+          type="button"
+          data-toggle="collapse"
+          data-target="#collapsibleNavbar"
+        >
+          <span
+            style={{ backgroundColor: 'black' }}
+            class="navbar-toggler-icon"
+          ></span>
+        </button>
 
-  <div class="collapse navbar-collapse " id="collapsibleNavbar">
-    <ul class="navbar-nav" style={{justifyContent:'center',alignItems:'center'}}>
-      <li class="nav-item">
-        <Link style={{color:'black'}}class="nav-link" to="/symptoms">Treatment and Symptom Report</Link>
-      </li>
-      <li class="nav-item">
-        <Link style={{color:'black'}} class="nav-link" to="/usersearch">Resource Inventory</Link>
-      </li>
-      <li class="nav-item">
-        <Link style={{color:'black'}} class="nav-link" to="/chronic">Chronic Disease Detection</Link>
-      </li>
-      {
-        userRole!=='USER' && isloggedIn ?<li class="nav-item">
-        <Link style={{color:'black'}} class="nav-link" to="/resources">medical Resources</Link>
-      </li>:null
-      }
-      
-      {
-        isloggedIn?<Link style={{color:'black'}} class="nav-link" to="/">Logout</Link>:<div >
-          <li class="nav-item" class='afterLogin d-flex justify-content-center align-items-center flex-row'>
-        <Link style={{color:'black'}} class="nav-link" to="/LoginUi">Login</Link>
-        <div class="dropdown">
-  <button style={{fontSize:'13px'}} class="btn dropdown-toggle nav-link" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Register
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-  <Link class="nav-link" to="/UserRegistration">user Registration</Link>
-  <Link class="nav-link" to="/Hospitalregistration">Hospital Registration</Link>
-  </div>
-</div>
-      </li>
+        <div class="collapse navbar-collapse " id="collapsibleNavbar">
+          <ul
+            class="navbar-nav"
+            style={{ justifyContent: 'center', alignItems: 'center' }}
+          >
+            <li className="nav-item">
+              <Link
+                style={{ color: 'black' }}
+                className="nav-link"
+                to="/symptoms"
+              >
+                Treatment and Symptom Report
+              </Link>
+            </li>
+
+            <li class="nav-item">
+              <Link style={{ color: 'black' }} class="nav-link" to="/chronic">
+                Chronic Disease Detection
+              </Link>
+            </li>
+
+            {isloggedIn ? (
+              <Link style={{ color: 'black' }} class="nav-link" to="/">
+                Logout
+              </Link>
+            ) : (
+              <div>
+                <li
+                  class="nav-item"
+                  className="afterLogin d-flex justify-content-center align-items-center flex-row"
+                >
+                  <Link
+                    style={{ color: 'black' }}
+                    class="nav-link"
+                    to="/LoginUi"
+                  >
+                    Login
+                  </Link>
+                  <Link
+                    style={{ color: 'black' }}
+                    class="nav-link"
+                    to="/UserRegistration"
+                  >
+                    Register
+                  </Link>
+                </li>
+              </div>
+            )}
+          </ul>
         </div>
-      }
-
-     
-      
-    </ul>
-  </div>
-</nav>
-        </>
-    )
-}
+      </nav>
+    </>
+  );
+};
 
 export default Navbar;
